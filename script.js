7e5b6df6650ed50be356960538a15c568020339d
mfortunat0 <matheus.xmaz10@gmail.com>
2023-10-10 14:54:40 -0300

    modal finalizado

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Hello!');
+window.addEventListener('resize', () => console.log(window.innerWidth));
+
+const modalCloseButton = document.querySelector('.modalClose');
+const modal = document.querySelector('.modal');
+const productButton = document.querySelectorAll('.productButton');
+
+modalCloseButton.addEventListener(
+  'click',
+  () => (modal.style.display = 'none')
+);
+
+productButton.forEach((button) => {
+  button.addEventListener('click', () => (modal.style.display = 'block'));
+});
